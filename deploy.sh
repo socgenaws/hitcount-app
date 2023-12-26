8617c4738f42b3e0bbaa72642b56b2c7008a9fb4
ravi <ravi@vytec.com>
2023-12-26 11:43:06 +0530

    added

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+if [ -d "hitcount-app" ]; then
+  echo "hitcount-app does exist."
+  rm -rf hitcount-app
+fi
 git clone https://github.com/socgenaws/hitcount-app.git
 cd hitcount-app
+docker-compose stop
+docker-compose rm 
 docker-compose up -d
 docker-compose ps 
-rm -f hitcount-app
